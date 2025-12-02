5ee12dc1dbcbc79717107d0fb0a5cb045f965000
Pericles Nahou <nahou241@CAMPUS@V304V-NCP036.campus.unicaen.fr>
2025-12-02 02:26:38 +0100

    Instruction supp

--- api/config.js
+++ api/config.js
@@ -3,7 +3,7 @@
 // This is useful when you don't have access to the university network
 
 export const API_CONFIG = {
-  USE_MOCK_API: true, // Change to false when connected to university network
+  USE_MOCK_API: false, // Change to false when connected to university network
   REAL_API_URL: 'http://graphql.unicaen.fr:4000',
   MOCK_DELAY: 500 // Simulated network delay in milliseconds
 }
